5894e8013644514ead182c9b4a38c269b9d46932
Luis Arthur <lass10@aluno.ifal.edu.br>
2026-06-04 13:12:26 -0300

    js

---
+++ index.js
@@ -0,0 +1,33 @@
+const slides = document.querySelectorAll (".slides img");
+let slideIndex =0;
+let intervaloId =null;
+
+initSlider();
+
+function initSlider(){
+   slides[slideIndex].classList.add("mostreSlide");
+   intervaloId=setInterval(nextSlide,5000);
+} 
+function mostreSlide(index){ 
+   if(index >=slides.length){
+      slideIndex =0;
+   }
+   else if(index<0){
+      slideIndex = slides.length -1;
+
+   }
+    slides.forEach(slide=>{
+      slide.classList.remove("mostreSlide");
+    });
+    slides[slideIndex].classList.add("mostreSlide");
+}
+function prevSlide(){
+   clearInterval(intervaloId);
+    slideIndex--;
+    mostreSlide(slideIndex);
+}
+function nextSlide(){
+   slideIndex++;
+   mostreSlide(slideIndex);
+
+}
